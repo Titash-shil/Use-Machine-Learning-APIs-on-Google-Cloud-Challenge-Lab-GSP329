877cd2eca6a06efa9e93f5f5363efc4f1e12e0cc
Titash-2807 <155870248+Titash-shil@users.noreply.github.com>
2025-02-04 21:06:27 +0530

    Like. comment, share & don't forget to Subscribe to {https://youtube.com/@titashshil?si=g0znCfbVs5zRle1D}(Qwiklab_Explorers_TS)

--- qwiklab_explorers_gsp329.sh
+++ qwiklab_explorers_gsp329.sh
@@ -3,17 +3,17 @@
 
 # Set text styles
 GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
 BLUE=`tput setaf 4`
-MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
 BOLD=$(tput bold)
 RESET=$(tput sgr0)
 
 echo "Please set the below values correctly from your lab instruction"
 read -p "${GREEN}${BOLD}Enter the LANGUAGE: ${RESET}" LANGUAGE
-read -p "${YELLOW}${BOLD}Enter the LOCALE: ${RESET}" LOCALE
-read -p "${BLUE}${BOLD}Enter the BIGQUERY_ROLE: ${RESET}" BIGQUERY_ROLE
-read -p "${MAGENTA}${BOLD}Enter the CLOUD_STORAGE_ROLE: ${RESET}" CLOUD_STORAGE_ROLE
+read -p "${BLUE}${BOLD}Enter the LOCALE: ${RESET}" LOCALE
+read -p "${CYAN}${BOLD}Enter the BIGQUERY_ROLE: ${RESET}" BIGQUERY_ROLE
+read -p "${WHITE}${BOLD}Enter the CLOUD_STORAGE_ROLE: ${RESET}" CLOUD_STORAGE_ROLE
 
 # Export variables after collecting input
 export LANGUAGE LOCALE BIGQUERY_ROLE CLOUD_STORAGE_ROLE
